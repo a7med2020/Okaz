c18c2d82bacd5a01ad7846ab0e90577126e2858a
Ahmed Mohamed Mustafa <a7med_2011209@yahoo.com>
2022-01-24 02:01:11 +0200

    AddIdentity01

---
+++ Okaz.Core/Entities/_Base/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Okaz.Core.Entities.Base
+{
+    class BaseEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key, Column(Order = 0)]
+        public int ID { get; set; }
+        public bool IsDeleted { get; set; }
+        public int CreatedBy { get; set; }
+        public int? ModefiedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModefiedDate { get; set; }
+       
+    }
+}
---
+++ Okaz.Infrastructure/Contexts/OkazContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Okaz.Infrastructure.Contexts
+{
+    class OkazContext : IdentityDbContext
+    {
+        public OkazContext(DbContextOptions<OkazContext> options) : base(options)
+        {
+
+        }
+
+        //public DbSet<Employee.Core.Entities.Employee> Employees { get; set; }
+    }
+}
