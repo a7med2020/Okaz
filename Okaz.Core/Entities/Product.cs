344e7f3c10e00a94e93e6052222c85aac9413403
Ahmed Mohamed Mustafa <a7med_2011209@yahoo.com>
2022-03-30 21:47:00 +0200

    AddProductEntity01

---
+++ Okaz.Core/Entities/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Okaz.Core.Entities
+{
+    public class Product
+    {
+        [StringLength(25)]
+        public string BarCode { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string Name { get; set; }
+
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+
+        [ForeignKey("ItemType")]
+        public string TypeID { get; set; }
+
+        public int ReorderLevel { get; set; }
+        public bool IsAddedFromExcel { get; set; }
+        public bool IsEnabled { get; set; }
+
+        public bool IsTaxedItem { get; set; }
+
+        [ForeignKey("Tax")]
+        public int? TaxID { get; set; }
+
+        [ForeignKey("ItemColor")]
+        public int? ColorID { get; set; }
+        [ForeignKey("ItemSize")]
+        public int? SizeID { get; set; }
+
+        public string Description { get; set; }
+        public string StorageLocation { get; set; }
+        public string ImagePath { get; set; }
+
+    }
+}
